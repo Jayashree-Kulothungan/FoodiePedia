2ab4037d6592c1efe79c8939a3127ef53c5a6173
Jayashree-Kulothungan <jayashreekulothungan@gmail.com>
2023-03-30 22:28:13 -0300

    updating registration

--- client/src/UserPool.js
+++ client/src/UserPool.js
@@ -1,8 +1,10 @@
-import { CognitoUserPool } from 'amazon-cognito-identity-js';
+var AmazonCognitoIdentity = require("amazon-cognito-identity-js");
+var CognitoUserPool = AmazonCognitoIdentity.CognitoUserPool;
 
+//userpool name : Registration
 const poolData = {
-  UserPoolId: 'us-east-1_QXtiv8zzH',
-  ClientId: 'sq62jkbp446h23dsfgdrakh84'
+	UserPoolId: "us-east-1_qQRBlIuTM",
+	ClientId: "gqab90ps3tcfo0ok589sba80d",
 };
 
 export default new CognitoUserPool(poolData);
