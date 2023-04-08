aa9e72fb392287ea223a5f1c08828fa9d8d2ab35
aishwaryan <as419275@dal.ca>
2023-04-08 01:23:13 -0300

    User Management Final

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,79 +1,202 @@
 import React, { useState } from 'react';
 import { CognitoUserPool, CognitoUser, AuthenticationDetails } from 'amazon-cognito-identity-js';
-import { Typography, Link } from '@material-ui/core';
+import { Typography, Link } from "@material-ui/core";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useNavigate } from "react-router-dom";
+
 
 const Login = ({ handleChange }) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
   const poolData = {
-    UserPoolId: 'us-east-1_tZ4v5GsGx',
-    ClientId: '5r7hias527h55b1mvml5qg2lvg'
+    UserPoolId: "us-east-1_ejWiyq9E0",
+    ClientId: "3m5b119nb903msrjh7jl6pp12j",
   };
 
   const userPool = new CognitoUserPool(poolData);
-
-  const onSubmit = e => {
+  const theme = createTheme();
+  const handleSubmit = (e) => {
     e.preventDefault();
 
     const authenticationData = {
       Username: email,
-      Password: password
+      Password: password,
     };
 
     const user = new CognitoUser({
       Username: email,
-      Pool: userPool
+      Pool: userPool,
     });
 
     const authDetails = new AuthenticationDetails(authenticationData);
 
     user.authenticateUser(authDetails, {
-      onSuccess: data => {
-        console.log('Login success:', data);
-		<Link to="/restaurant"></Link>
+      onSuccess: (data) => {
+        const user = data["accessToken"]["payload"]["username"];
+        localStorage.setItem("user", user);
+        navigate("/home");
+        window.location.reload();
       },
-      onFailure: err => {
-        console.error('Login failure:', err);
-        setErrorMessage(err.message);
-      }
+      onFailure: (err) => {
+        console.error("Login failure:", err);
+      },
     });
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      {errorMessage && <div className="error">{errorMessage}</div>}
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-          required
-        />
-      </div>
-	  <button type="submit" onClick={(e) => {e.preventDefault(); window.location.href="/restaurants"}}>Login</button>
-      <Typography > Do you have an account ?
-           <Link href="/register" onClick={() => handleChange("event", 1)} >
-                        Register
-            </Link>
-      </Typography>
-    </form>
+    <>
+      <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box
+              component="form"
+              onSubmit={handleSubmit}
+              noValidate
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              <Grid container justifyContent="flex-end">
+                <Grid item>
+                  <Link href="/register" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+        </Container>
+      </ThemeProvider>
+    </>
   );
 };
+
+// const Login = ({ handleChange }) => {
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
+//   const [errorMessage, setErrorMessage] = useState('');
+
+//   const poolData = {
+//     UserPoolId: 'us-east-1_tZ4v5GsGx',
+//     ClientId: '5r7hias527h55b1mvml5qg2lvg'
+//   };
+
+//   const userPool = new CognitoUserPool(poolData);
+
+//   const onSubmit = e => {
+//     e.preventDefault();
+
+//     const authenticationData = {
+//       Username: email,
+//       Password: password
+//     };
+
+//     const user = new CognitoUser({
+//       Username: email,
+//       Pool: userPool
+//     });
+
+//     const authDetails = new AuthenticationDetails(authenticationData);
+
+//     user.authenticateUser(authDetails, {
+//       onSuccess: data => {
+//         console.log('Login success:', data);
+// 		<Link to="/restaurant"></Link>
+//       },
+//       onFailure: err => {
+//         console.error('Login failure:', err);
+//         setErrorMessage(err.message);
+//       }
+//     });
+//   };
+
+//   return (
+//     <form onSubmit={onSubmit}>
+//       {errorMessage && <div className="error">{errorMessage}</div>}
+//       <div>
+//         <label htmlFor="email">Email:</label>
+//         <input
+//           type="email"
+//           id="email"
+//           name="email"
+//           value={email}
+//           onChange={e => setEmail(e.target.value)}
+//           required
+//         />
+//       </div>
+//       <div>
+//         <label htmlFor="password">Password:</label>
+//         <input
+//           type="password"
+//           id="password"
+//           name="password"
+//           value={password}
+//           onChange={e => setPassword(e.target.value)}
+//           required
+//         />
+//       </div>
+// 	  <button type="submit" onClick={(e) => {e.preventDefault(); window.location.href="/restaurants"}}>Login</button>
+//       <Typography > Do you have an account ?
+//            <Link href="/register" onClick={() => handleChange("event", 1)} >
+//                         Register
+//             </Link>
+//       </Typography>
+//     </form>
+//   );
+// };
 
 export default Login;
--- client/src/components/register.jsx
+++ client/src/components/register.jsx
@@ -21,11 +21,6 @@ export default function SignUp() {
 
 	const handleSubmit = (event) => {
 		event.preventDefault();
-		const data = new FormData(event.currentTarget);
-		setEmail(data.get("email"));
-		setPassword(data.get("password"));
-		setGivenName(data.get("firstName"));
-		setfamilyName(data.get("lastName"));
 		var attributeList = [];
 		var dataEmail = {
 			Name: "email",
@@ -89,6 +84,8 @@ export default function SignUp() {
 									id="firstName"
 									label="First Name"
 									autoFocus
+									value={givenName}
+                                    onChange={(e) => setGivenName(e.target.value)}
 								/>
 							</Grid>
 							<Grid item xs={12} sm={6}>
@@ -99,6 +96,8 @@ export default function SignUp() {
 									label="Last Name"
 									name="lastName"
 									autoComplete="family-name"
+									value={familyName}
+                                    onChange={(e) => setfamilyName(e.target.value)}
 								/>
 							</Grid>
 							<Grid item xs={12}>
@@ -109,6 +108,7 @@ export default function SignUp() {
 									label="Email Address"
 									name="email"
 									autoComplete="email"
+									onChange={(e) => setEmail(e.target.value)}
 								/>
 							</Grid>
 							<Grid item xs={12}>
@@ -120,6 +120,8 @@ export default function SignUp() {
 									type="password"
 									id="password"
 									autoComplete="new-password"
+									value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
 								/>
 							</Grid>
 						</Grid>
@@ -133,7 +135,7 @@ export default function SignUp() {
 						</Button>
 						<Grid container justifyContent="flex-end">
 							<Grid item>
-								<Link href="#" variant="body2">
+								<Link href="/login" variant="body2">
 									Already have an account? Sign in
 								</Link>
 							</Grid>
